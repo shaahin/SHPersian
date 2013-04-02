2cc5c9e710bd2063f8a9431182808dd6bc39886d
Shahin <me@shaahin.us>
2013-04-02 15:59:37 +0430

    Initial Commit. Including SHPersian category and SHLabel

---
+++ SHPersianSample/SHPersianSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SHPersianSample
+//
+//  Created by Shahin Katebi on 4/2/13.
+//  Copyright (c) 2013 Shaahin.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SHPersianSample/SHPersianSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SHPersianSample
+//
+//  Created by Shahin Katebi on 4/2/13.
+//  Copyright (c) 2013 Shaahin.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
